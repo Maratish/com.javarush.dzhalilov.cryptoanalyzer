f9de2dc1f848291e0fb3488869d0432e2b1f729c
Marat Dzhalilov <dzhalilovmarra@yandex.ru>
2024-09-27 10:50:27 +0300

    Дешифровка и шифровка

--- src/FIleManager.java
+++ src/FIleManager.java
@@ -1,74 +1,32 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Date;
 import java.util.Scanner;
 
 public class FIleManager {
 
     public static String readFile() {
         StringBuilder originalText = new StringBuilder();
-        Scanner scanner = new Scanner(System.in);
-        String strInputFile = null;
         System.out.println("Введите путь к исходному документу:");
-        while (strInputFile == null) {
-            strInputFile = scanner.nextLine();
-            Path path = Paths.get(strInputFile);
-            if (!Files.exists(path)) {
-                System.out.println("Файла не существует. Повторите ввод");
-                strInputFile = null;
-            } else {
-                break;
-            }
-        }
-        try (BufferedReader bfr = new BufferedReader(new FileReader(strInputFile))){
+        String strInputFile = Validator.validateInputPath(new Scanner(System.in));
+        try (BufferedReader bfr = new BufferedReader(new FileReader(strInputFile))) {
             String s;
             while ((s = bfr.readLine()) != null) {
                 originalText.append(s);
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Ошибка"+e);;
         }
         return originalText.toString();
     }
 
-    public static void writeFile(String writingText) {
-        Scanner scanner = new Scanner(System.in);
-        String strOutputFile = null;
-        System.out.println("Куда сохранить результат?:");
-        while (strOutputFile == null) {
-            strOutputFile = scanner.nextLine();
-            try {
-                Path pathOutputFile = Paths.get(strOutputFile);
-                if (Files.exists(pathOutputFile) && Files.isDirectory(pathOutputFile)) {
-                    Path newPath = Paths.get(pathOutputFile.toString() +"\\newDocument.txt");
-                    if (!Files.exists(newPath)) {
-                        Files.createFile(newPath);
-                        strOutputFile = newPath.toString();
-                        break;
-                    } else {
-                        System.out.println("Файл уже существует");
-                        strOutputFile=null;
-                        continue;
-                    }
-                } if (Files.exists(pathOutputFile) && Files.isRegularFile(pathOutputFile)) {
-                    break;
-                } else {
-                    System.out.println("Введите корректный путь");
-                    strOutputFile=null;
-                }
-            } catch (InvalidPathException | IOException e) {
-                strOutputFile = null;
-                System.out.println("Введите корректный путь");
-            }
-        }
 
-        try (BufferedWriter bfwr = new BufferedWriter(new FileWriter(strOutputFile))){
+    public static void writeFile(String writingText) {
+
+        System.out.println("Куда сохранить результат?:");
+        String strOutputFile = Validator.validateOutputPath(new Scanner(System.in));
+
+        try (BufferedWriter bfwr = new BufferedWriter(new FileWriter(strOutputFile))) {
             bfwr.write(writingText);
-        } catch (RuntimeException| IOException e) {
+        } catch (RuntimeException | IOException e) {
             System.out.println("Ошибка" + e);
         }
     }
--- src/Validator.java
+++ src/Validator.java
@@ -1,22 +1,83 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Validator {
-    public static int validateKey (Scanner scanner){
+    public static int validateKey(Scanner scanner) {
         int key = -1;
         while (true) {
             try {
-                key=scanner.nextInt();
-                if (!(key <= 0 || key > (Alphabet.alphabet_Map.size()-1))) {
+                key = scanner.nextInt();
+                if (!(key <= 0 || key > (Alphabet.alphabet_Map.size() - 1))) {
                     break;
                 } else {
-                    System.out.println("Введите число от 0 до " + (Alphabet.alphabet_Map.size()-1));
+                    System.out.println("Введите число от 0 до " + (Alphabet.alphabet_Map.size() - 1));
                 }
             } catch (InputMismatchException | NumberFormatException e) {
                 scanner.nextLine();
-                System.out.println("Введите число от 0 до " + (Alphabet.alphabet_Map.size()-1));
+                System.out.println("Введите число от 0 до " + (Alphabet.alphabet_Map.size() - 1));
             }
         }
         return key;
     }
+
+    public static String validateInputPath(Scanner scanner) {
+        String strInputFile = null;
+        while (strInputFile == null) {
+            try {
+                strInputFile = scanner.nextLine();
+                Path path = Paths.get(strInputFile);
+                if (!Files.isRegularFile(path)) {
+                    System.out.println("Файла не существует. Повторите ввод");
+                    strInputFile = null;
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                strInputFile=null;
+                System.out.println("Ошибка "+e.getMessage()+" .Введите корректный путь");
+
+            }
+        }
+        return strInputFile;
+    }
+
+    public static String validateOutputPath(Scanner scanner) {
+        String strOutputFile = null;
+        while (strOutputFile == null) {
+            strOutputFile = scanner.nextLine();
+            try {
+                Path pathOutputFile = Paths.get(strOutputFile);
+                if (Files.exists(pathOutputFile) && Files.isDirectory(pathOutputFile)) {
+                    System.out.println("Придумайте название нового файла:");
+                    String newName = scanner.nextLine();
+                    Path newPath = Paths.get(pathOutputFile + newName+".txt");
+                    if (!Files.exists(newPath)) {
+                        Files.createFile(newPath);
+                        strOutputFile = newPath.toString();
+                        break;
+                    } else {
+                        System.out.println("Файл уже существует");
+                        strOutputFile = null;
+                        continue;
+                    }
+                }
+                if (Files.exists(pathOutputFile) && Files.isRegularFile(pathOutputFile)) {
+                    break;
+                } else {
+                    System.out.println("Введите корректный путь");
+                    strOutputFile = null;
+                }
+            } catch (InvalidPathException | IOException e) {
+                strOutputFile = null;
+                System.out.println("Ошибка "+e.getMessage()+" .Введите корректный путь");
+            }
+        }
+        return strOutputFile;
+    }
 }
+
